93c9a8ffa33e403ebef5723d2fe476ab947da0b1
tobeneck <tobias.benecke@ovgu.de>
2024-06-13 15:22:39 +0200

    now generating one initial pop for each problem separately

--- code/generate_initial_pop.py
+++ code/generate_initial_pop.py
@@ -4,6 +4,13 @@ This file generates the initial populations used for the tests with the Sphere, 
 import os
 import numpy as np
 
+from pymoo.operators.sampling.rnd import FloatRandomSampling
+
+from pymoo_problems.moo.maco import MACO
+from pymoo_problems.moo.uf import UF1, UF2, UF3
+from pymoo_problems.moo.zdt import ZDT1, ZDT2, ZDT3
+from pymoo_problems.moo.dtlz import DTLZ1, DTLZ2, DTLZ3
+
 #check if the output path exists
 out_path = "../data/initial_populations"
 if not os.path.exists(out_path):
@@ -14,13 +21,37 @@ np.random.seed(1)
 
 #generate the initial populations
 pop_size = 91
-search_space_dims = [10]
-objective_space_dims = [2, 3]
+n_var = 10
+
+problems = {
+    #dtlz
+    "dtlz1": DTLZ1(n_var=n_var, n_obj=3),
+    "dtlz2": DTLZ2(n_var=n_var, n_obj=3),
+    "dtlz3": DTLZ3(n_var=n_var, n_obj=3),
+
+    #zdt
+    "zdt1": ZDT1(n_var=n_var),
+    "zdt2": ZDT2(n_var=n_var),
+    "zdt3": ZDT3(n_var=n_var),
+
+    #uf
+    "uf1": UF1(n_var=n_var),
+    "uf2": UF2(n_var=n_var),
+    "uf3": UF3(n_var=n_var),
+
+    #maco
+    "maco_basic": MACO(n_var=n_var, p=-np.inf, weights=None, classes=None, ctype=None, wtype=None),
+    "maco_p-norm": MACO(n_var=n_var, p=2, weights=None, classes=None, ctype=None, wtype=None),
+    "maco_weights": MACO(n_var=n_var, p=-np.inf, weights=None, classes=None, ctype=None, wtype="steep"),
+    "maco_classes": MACO(n_var=n_var, p=-np.inf, weights=None, classes=None, ctype="half", wtype=None),
+}
 
 '''
-NOTE: The same initial population is used for all problems
+generate one initial population for each problem separately because of bounds.
 '''
-for d_s in search_space_dims:
-   for d_o in objective_space_dims:
-      random_pop = (np.random.random((pop_size-d_o-1, d_s))) #range from 0.0 to (almoust) 1.0
-      np.savetxt(out_path+"/random_pop_ds"+str(d_s)+"_do"+str(d_o)+".csv", random_pop, delimiter=",")
+for problem_name in problems.keys():
+   problem = problems[problem_name]
+   sampling = FloatRandomSampling()
+   X = sampling._do(problem, pop_size)
+   print(X)
+   np.savetxt(out_path+"/random_pop_"+problem_name+"_ds"+str(problem.n_var)+"_do"+str(problem.n_obj)+".csv", X, delimiter=",")
